4bc8704514578524dfb374d180d443241d24d7a9
David <ddevargas@lean-tech.io>
2020-06-02 16:19:40 -0500

    Solving error not such credentials file

--- procedures/get_credentials.py
+++ procedures/get_credentials.py
@@ -22,7 +22,7 @@ def get_credentials():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('../../credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('../credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
 
         # save the credentials for the next run
